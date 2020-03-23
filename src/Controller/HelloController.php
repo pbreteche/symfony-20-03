d3c39c86156d9950098eb601723714a1223fe003
Pierre Bretéché <pbreteche@dawan.fr>
2020-03-23 15:57:54 +0100

    création page conversion km->miles

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController
+class HelloController extends AbstractController
 {
+    public const KILOMETERS_PER_MILES = 1.609;
 
     public function hello()
     {
@@ -17,4 +19,13 @@ class HelloController
         return $response;
     }
 
+    public function convert($kilometers)
+    {
+        $miles = $kilometers / self::KILOMETERS_PER_MILES;
+
+        return $this->json([
+            'kilometers' => $kilometers,
+            'miles' => $miles,
+        ]);
+    }
 }
